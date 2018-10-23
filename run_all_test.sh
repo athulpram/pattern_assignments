cc853da34c3acee85a4cfb0556ac1886203e86d6
praathul <22594358+athulpkr98@users.noreply.github.com>
2018-10-23 18:04:42 +0530

    Seperate test script , arrange directory structure
    Seperate the test scripts in to two
      -> run_app_test.sh
      -> run_unit_test.sh
    
    Moved inputs and outputs into directory called app_test_data.
    moved scripts in to directory called scripts.

--- run_all_test.sh
+++
@@ -1,5 +0,0 @@
-node test/patternUtilsTest.js
-node test/patternsLibraryTest.js
-./run_test.sh draw_rectangle.js ./test/rectangle_input ./test/rectangle_exp_out
-./run_test.sh draw_diamond.js ./test/diamonds_input ./test/diamonds_output
-./run_test.sh draw_triangle.js ./test/triangle_input ./test/triangle_output
---
+++ scripts/run_unit_test.sh
@@ -0,0 +1,2 @@
+node test/patternUtilsTest.js
+node test/patternsLibraryTest.js
