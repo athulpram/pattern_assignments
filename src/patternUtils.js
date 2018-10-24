930db29eced08ac41f5feb9b6a413c6545e0bbd1
praathul <22594358+athulpkr98@users.noreply.github.com>
2018-10-24 17:09:11 +0530

    Pull private functions from Utils , Replace if with Object
    Pulled private functions from pattern utilsLibrary to patterns library
    
    Replaced if conditions in type of Rectangle and Triangle with objects

--- src/patternUtils.js
+++ src/patternUtils.js
@@ -6,41 +6,6 @@ const repeatCharacter = function(character,width){
   return line;
 }
 
-const generateFilledRectangle = function(height,width){
-  let rectangle = "";
-  let delimiter ="";
-  for(let line=0;line<height;line++){
-    rectangle+=delimiter+repeatCharacter("*",width);
-    delimiter="\n";
-  }
-  return rectangle;
-}
-
-const generateEmptyRectangle=function(height,width){
-  let rectangle = repeatCharacter("*",width);
-  let delimiter = "\n";
-  for(let line=0;line<height-2;line++){
-    rectangle+=delimiter+"*"+repeatCharacter(" ",width-2)+"*";
-  }
-  rectangle +=delimiter+repeatCharacter("*",width);
-  return rectangle;
-}
-
-const generateAlternatingRectangle = function(height,width){
-
-  let rectangle = "";
-  let delimiter = "";
-  for(let line=0;line<height;line++){
-    rectangle+=delimiter+repeatCharacter("*",width);
-    line++;
-    delimiter="\n";
-    if(line<height){
-      rectangle+=delimiter+repeatCharacter("-",width);
-    }
-  }
-  return rectangle;
-}
-
 
 const generateLine = function (width,lineNumber,isRight){
   let line= "";
@@ -51,16 +16,6 @@ const generateLine = function (width,lineNumber,isRight){
   return line;
 }
 
-const generateTriangle = function(isRight,height){
-  delimiter="";
-  triangle="";
-  for(lineNumber=1;lineNumber<=height;lineNumber++){
-    triangle+=delimiter+generateLine(height,lineNumber,isRight);
-    delimiter="\n";
-  }
-  return triangle;
-}
-
 
 const addSpaces = function(line,width){
   while(line.length<width){
@@ -68,66 +23,6 @@ const addSpaces = function(line,width){
   }
   return line;
 }
-
-const generateDiamond = function(firstCharacter,lastCharacter,characterToFill,lineNumber,width){
-  let line=firstCharacter;
-  let spaces="";
-  let timesToFill = 2*lineNumber-3;
-  line += repeatCharacter(characterToFill,timesToFill);
-  line += lastCharacter;
-  let numberOfSpaces =(width-line.length)/2;
-  spaces += repeatCharacter(" ",numberOfSpaces);
-  if(lineNumber==1||lineNumber==width){
-    line=firstCharacter;
-  }
-  return spaces+line+spaces;
-}
-
-const generateAngledDiamond=function (height){
-  let diamond="";
-  let spaces="";
-  for(let characterIndex=1;characterIndex<(height/2);characterIndex++){
-    spaces+=" ";
-  }
-  diamond=spaces+"*"+spaces+"\n";
-  for(let lineNumber=2;lineNumber<height/2;lineNumber++){
-    diamond+=generateDiamond("/","\\"," ",lineNumber,height)+"\n";
-  }
-  diamond+=generateDiamond("*","*"," ",Math.floor(height/2+1),height);
-  for(let lineNumber=Math.floor(height/2);lineNumber>1;lineNumber--){
-    diamond+="\n"+generateDiamond("\\","/"," ",lineNumber,height);
-  }
-  diamond+="\n"+spaces+"*"+spaces;
-  return diamond;
-}
-
-const generateHollowDiamond = function(height){
-  let diamond="";
-  let delimiter="";
-  for(let lineNumber=1;lineNumber<=Math.ceil(height/2);lineNumber++){
-    diamond+=delimiter+generateDiamond("*","*"," ",lineNumber,height);
-    delimiter = "\n";
-  }
-  for(let lineNumber=Math.ceil(height/2-1);lineNumber>0;lineNumber--){
-    diamond+=delimiter+generateDiamond("*","*"," ",lineNumber,height);
-  }
-  return diamond;
-} 
-
-const generateFilledDiamond = function(height){
-  let diamond="";
-  let delimiter="";
-  for(let lineNumber=1;lineNumber<=Math.ceil(height/2);lineNumber++){
-    diamond+=delimiter+generateDiamond("*","*","*",lineNumber,height);
-    delimiter = "\n";
-  }
-  for(let lineNumber=Math.ceil(height/2-1);lineNumber>0;lineNumber--){
-    diamond+=delimiter+generateDiamond("*","*","*",lineNumber,height);
-  }
-  return diamond;
-
-}
-
 const validateHeight=function(height){
   if(height%2==0){
     return height-1;
@@ -136,14 +31,6 @@ const validateHeight=function(height){
 } 
 
 exports.repeatCharacter = repeatCharacter;
-exports.generateFilledRectangle =generateFilledRectangle;
-exports.generateEmptyRectangle = generateEmptyRectangle;
-exports.generateAlternatingRectangle =generateAlternatingRectangle;
 exports.generateLine = generateLine;
-exports.generateTriangle = generateTriangle;
 exports.addSpaces = addSpaces;
-exports.generateDiamond = generateDiamond;
-exports.generateHollowDiamond = generateHollowDiamond;
-exports.generateAngledDiamond = generateAngledDiamond;
-exports.generateFilledDiamond = generateFilledDiamond;
 exports.validateHeight = validateHeight;
--- src/patternsLibrary.js
+++ src/patternsLibrary.js
@@ -1,31 +1,68 @@
-const {repeatCharacter,
-  generateFilledRectangle,
-  generateEmptyRectangle,
-  generateAlternatingRectangle,
+const {
+  repeatCharacter,
   generateLine,
-  generateTriangle,
   addSpaces,
-  generateFilledDiamond,
-  generateAngledDiamond,
-  generateDiamond,
-  generateHollowDiamond,
-  validateHeight}=require("./patternUtils.js");
+  validateHeight
+} = require("./patternUtils.js");
 
-const createRectangleOfType = function(typeOfRect,width,height){
+const generateFilledRectangle = function(height,width){
   let rectangle = "";
-  if(typeOfRect=="filled"){
-    rectangle=generateFilledRectangle(height,width);
-  }
-
-  if(typeOfRect=="empty"){
-    rectangle=generateEmptyRectangle(height,width);
-  }
-
-  if(typeOfRect=="alternating"){
-    rectangle=generateAlternatingRectangle(height,width);
+  let delimiter ="";
+  for(let line=0;line<height;line++){
+    rectangle+=delimiter+repeatCharacter("*",width);
+    delimiter="\n";
   }
   return rectangle;
 }
+
+const generateEmptyRectangle=function(height,width){
+  let rectangle = repeatCharacter("*",width);
+  let delimiter = "\n";
+  for(let line=0;line<height-2;line++){
+    rectangle+=delimiter+"*"+repeatCharacter(" ",width-2)+"*";
+  }
+  rectangle +=delimiter+repeatCharacter("*",width);
+  return rectangle;
+}
+
+const generateAlternatingRectangle = function(height,width){
+
+  let rectangle = "";
+  let delimiter = "";
+  for(let line=0;line<height;line++){
+    rectangle+=delimiter+repeatCharacter("*",width);
+    line++;
+    delimiter="\n";
+    if(line<height){
+      rectangle+=delimiter+repeatCharacter("-",width);
+    }
+  }
+  return rectangle;
+}
+
+
+
+const createRectangleOfType = function(typeOfRect,width,height){
+  rectangleMapping = {
+    "filled" : generateFilledRectangle,
+    "empty" : generateEmptyRectangle,
+    "alternating" : generateAlternatingRectangle
+  }
+  
+  let rectangle = rectangleMapping[typeOfRect](height,width);
+  return rectangle;
+}
+
+const generateTriangle = function(isRight,height){
+  delimiter="";
+  triangle="";
+  for(lineNumber=1;lineNumber<=height;lineNumber++){
+    triangle+=delimiter+generateLine(height,lineNumber,isRight);
+    delimiter="\n";
+  }
+  return triangle;
+}
+
 
 const createTriangleOfType = function(triangleType,height){
   let isRight = false;
@@ -34,20 +71,76 @@ const createTriangleOfType = function(triangleType,height){
   }
   return (generateTriangle(isRight,height));
 }
-const generateDiamondOfType=function(typeOfDiamond,height){
+
+
+const generateDiamond = function(firstCharacter,lastCharacter,characterToFill,lineNumber,width){
+  let line=firstCharacter;
+  let spaces="";
+  let timesToFill = 2*lineNumber-3;
+  line += repeatCharacter(characterToFill,timesToFill);
+  line += lastCharacter;
+  let numberOfSpaces =(width-line.length)/2;
+  spaces += repeatCharacter(" ",numberOfSpaces);
+  if(lineNumber==1||lineNumber==width){
+    line=firstCharacter;
+  }
+  return spaces+line+spaces;
+}
+
+const generateAngledDiamond=function (height){
+  let diamond="";
+  let spaces="";
+  for(let characterIndex=1;characterIndex<(height/2);characterIndex++){
+    spaces+=" ";
+  }
+  diamond=spaces+"*"+spaces+"\n";
+  for(let lineNumber=2;lineNumber<height/2;lineNumber++){
+    diamond+=generateDiamond("/","\\"," ",lineNumber,height)+"\n";
+  }
+  diamond+=generateDiamond("*","*"," ",Math.floor(height/2+1),height);
+  for(let lineNumber=Math.floor(height/2);lineNumber>1;lineNumber--){
+    diamond+="\n"+generateDiamond("\\","/"," ",lineNumber,height);
+  }
+  diamond+="\n"+spaces+"*"+spaces;
+  return diamond;
+}
+
+const generateHollowDiamond = function(height){
   let diamond="";
   let delimiter="";
+  for(let lineNumber=1;lineNumber<=Math.ceil(height/2);lineNumber++){
+    diamond+=delimiter+generateDiamond("*","*"," ",lineNumber,height);
+    delimiter = "\n";
+  }
+  for(let lineNumber=Math.ceil(height/2-1);lineNumber>0;lineNumber--){
+    diamond+=delimiter+generateDiamond("*","*"," ",lineNumber,height);
+  }
+  return diamond;
+} 
+
+const generateFilledDiamond = function(height){
+  let diamond="";
+  let delimiter="";
+  for(let lineNumber=1;lineNumber<=Math.ceil(height/2);lineNumber++){
+    diamond+=delimiter+generateDiamond("*","*","*",lineNumber,height);
+    delimiter = "\n";
+  }
+  for(let lineNumber=Math.ceil(height/2-1);lineNumber>0;lineNumber--){
+    diamond+=delimiter+generateDiamond("*","*","*",lineNumber,height);
+  }
+  return diamond;
+
+}
+
+
+const generateDiamondOfType=function(typeOfDiamond,height){
   height = validateHeight(height);
-  if(typeOfDiamond=="hollow"){
-    diamond=generateHollowDiamond(height)
+  diamondMaps = {
+    "hollow" : generateHollowDiamond,
+    "angled" : generateAngledDiamond,
+    "filled" : generateFilledDiamond
   }
-  if(typeOfDiamond=="angled"){
-    diamond=generateAngledDiamond(height);
-  }
-  if(typeOfDiamond=="filled"){
-    diamond=generateFilledDiamond(height);
-    delimiter="\n";
-  }
+  let diamond=diamondMaps[typeOfDiamond](height)
   return diamond;
 }
 
