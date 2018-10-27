630c94aaa237365367580ee77b0fc2d74782ac25
praathul <44019979+athulpram@users.noreply.github.com>
2018-10-27 13:46:44 +0530

    Modify scripts to exit execution when error occurs
    Modified the run_app_test and run_unit_test to exit when an error
    occurs

--- scripts/run_app_test.sh
+++ scripts/run_app_test.sh
@@ -1,3 +1,4 @@
+set -e
 ./scripts/run_test.sh createRectangle.js ./app_test_data/inputs/rectangle_input ./app_test_data/outputs/rectangle_exp_out
 ./scripts/run_test.sh createDiamond.js ./app_test_data/inputs/diamonds_input ./app_test_data/outputs/diamonds_output
 ./scripts/run_test.sh createTriangle.js ./app_test_data/inputs/triangle_input ./app_test_data/outputs/triangle_output
--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -1,3 +1,4 @@
+set -e
 for TESTFILE in test/*Test.js
 do
   echo $TESTFILE
